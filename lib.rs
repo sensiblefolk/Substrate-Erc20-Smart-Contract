c3f41d9333401e15a3ad6f85318875d9904d8f38
michael anyi <mikeyeucresta@gmail.com>
2021-02-03 11:14:25 +0100

    added substrate erc20

---
+++ lib.rs
@@ -0,0 +1,180 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+use ink_storage::collections::HashMap;
+
+#[ink::contract]
+mod erc20 {
+    use super::*;
+
+    #[cfg(not(feature = "ink-as-dependency"))]
+    #[ink(storage)]
+    pub struct Erc20 {
+        /// The total supply.
+        total_supply: Balance,
+        /// The balance of each user.
+        balances: HashMap<AccountId, Balance>,
+
+        /// Balances that are spendable by non-owners: (owner, spender) -> allowed
+        allowances: HashMap<(AccountId, AccountId), Balance>,
+    }
+
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: Balance,
+    }
+
+    #[ink(event)]
+    pub struct Approval {
+        #[ink(topic)]
+        owner: AccountId,
+        #[ink(topic)]
+        spender: AccountId,
+        value: Balance,
+    }
+
+    impl Erc20 {
+        #[ink(constructor)]
+        pub fn new(initial_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = HashMap::new();
+            balances.insert(Self::env().caller(), initial_supply);
+
+            Self::env().emit_event(Transfer {
+                from: None,
+                to: Some(caller),
+                value: initial_supply,
+            });
+
+            Self {
+                total_supply: initial_supply,
+                balances,
+                allowances: HashMap::new(),
+            }
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
+        }
+
+        #[ink(message)]
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balance_of_or_zero(&owner)
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> bool {
+            // Record the new allowance
+            let owner = self.env().caller();
+            self.allowances.insert((owner, spender), value);
+
+            // Notify offchain users of the approval and report success.
+            self.env().emit_event(Approval {
+                owner,
+                spender,
+                value,
+            });
+            true
+        }
+
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowance_of_or_zero(&owner, &spender)
+        }
+
+        #[ink(message)]
+        pub fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
+            // Ensure that a sufficient allowance exists
+            let caller = self.env().caller();
+            let spend_allowance = self.allowance_of_or_zero(&from, &caller);
+            if spend_allowance < value {
+                return false;
+            };
+
+            // Decrease the value of the allowance and transfer the tokens.
+            self.allowances
+                .insert((from, caller), spend_allowance - value);
+            self.transfer_from_to(from, to, value)
+        }
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> bool {
+            let sender = self.env().caller();
+            self.transfer_from_to(sender, to, value)
+        }
+
+        fn transfer_from_to(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
+            let sender_balance = self.balance_of_or_zero(&from);
+            let receiver_balance = self.balance_of_or_zero(&to);
+
+            if sender_balance < value {
+                return false;
+            };
+
+            // Update the sender's balance
+            self.balances.insert(from, sender_balance - value);
+
+            // Update the receiver's balance
+            self.balances.insert(to, receiver_balance + value);
+
+            self.env().emit_event(Transfer {
+                from: Some(from),
+                to: Some(to),
+                value,
+            });
+            true
+        }
+
+        fn balance_of_or_zero(&self, owner: &AccountId) -> Balance {
+            *self.balances.get(owner).unwrap_or(&0)
+        }
+
+        fn allowance_of_or_zero(&self, owner: &AccountId, spender: &AccountId) -> Balance {
+            *self.allowances.get(&(*owner, *spender)).unwrap_or(&0)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn new_works() {
+            let contract = Erc20::new(777);
+            assert_eq!(contract.total_supply(), 777);
+        }
+
+        #[ink::test]
+        fn balance_works() {
+            let contract = Erc20::new(100);
+            assert_eq!(contract.total_supply(), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 0);
+        }
+
+        #[ink::test]
+        fn transfer_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            assert!(contract.transfer(AccountId::from([0x0; 32]), 10));
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
+            assert!(!contract.transfer(AccountId::from([0x0; 32]), 100));
+        }
+
+        #[ink::test]
+        fn transfer_from_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            contract.approve(AccountId::from([0x1; 32]), 20);
+            contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 10);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# An ERC20 Smart Contract With Substrate
+
+### Trying it out
+
+Simply go to [substrated.dev](https://substrate.dev/) and follow the [installation](https://substrate.dev/docs/en/knowledgebase/getting-started/) instructions.
